6bc6598c07e2f2c0d85a6229322b6100bda44018
nbur4556 <nbur4556@gmail.com>
2020-08-17 18:08:42 -0500

    can disable individual text inputs in the array

--- assets/scripts/mainscript.js
+++ assets/scripts/mainscript.js
@@ -1,5 +1,30 @@
 $(document).ready(function () {
     const m = moment();
 
+    blockPastTimes();
+
+    //Show current day to string
     $('#currentDay').text(m.format('dddd, MMMM Do'));
+
+    //Save Button Click Event
+    $('.timeblock button').click(saveSchedule);
+
+    //Disable all time blocks earlier than the current moment object
+    function blockPastTimes() {
+        let timeBlockInputs = $('.timeblock input');
+
+        for (let i = 0; i < timeBlockInputs.length; i++) {
+            timeBlockInputs[i].setAttribute('disabled', '');
+        }
+    }
+
+    //Save schedule to local storage
+    function saveSchedule() {
+        console.log("Save Schedule");
+    }
+
+    //Load schedule from local storage
+    function loadSchedule() {
+
+    }
 });
